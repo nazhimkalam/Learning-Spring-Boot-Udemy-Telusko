9ca578300ac80d8f6e72b2dc1be3fc8f6abe1566
Nazhim <nazhimkalam@gmail.com>
2020-09-27 23:43:55 +0530

    Used Model to add attributes

--- Demo 2/src/main/java/com/nazhimkalam/demo2/HomeController.java
+++ Demo 2/src/main/java/com/nazhimkalam/demo2/HomeController.java
@@ -1,7 +1,13 @@
 package com.nazhimkalam.demo2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -9,7 +15,16 @@ public class HomeController {
     @RequestMapping("/")
     public String home()
     {
-        return "index.jsp";
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public String addition(@RequestParam("num1") int firstNumber, @RequestParam("num2") int secondNumber, Model m)
+    {
+        int total = firstNumber + secondNumber;
+        m.addAttribute("total", total);
+
+        return "result";
     }
 
 }
--- Demo 2/src/main/webapp/views/result.jsp
+++ Demo 2/src/main/webapp/views/result.jsp
@@ -1,14 +1,15 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" isELIgnored="false" %>
 
 <!DOCTYPE html>
 <html>
+
     <head>
         <meta charset="UTF-8" >
         <title>Insert title over here</title>
     </head>
 
     <body>
-        Welcome to my channel
+        Result is ${total}
     </body>
 
 </html>
