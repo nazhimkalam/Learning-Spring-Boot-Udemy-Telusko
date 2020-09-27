574f7dbd2131535cb4c0bf16136cc29036aebde5
Nazhim <nazhimkalam@gmail.com>
2020-09-27 19:58:03 +0530

    back to the basic template of the spring starter project

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
 
-        Alien obj = context.getBean(Alien.class);   // spring will create an object for you using "getBean" method
-
-        obj.code();
     }
 }
