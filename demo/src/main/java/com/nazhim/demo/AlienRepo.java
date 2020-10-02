0e59f87ceb3794bb2fc1f055e959836554c67256
Nazhim <nazhimkalam@gmail.com>
2020-10-02 20:03:44 +0530

    Query Annotation

--- demo/src/main/java/com/nazhim/demo/AlienRepo.java
+++ demo/src/main/java/com/nazhim/demo/AlienRepo.java
@@ -4,10 +4,16 @@ package com.nazhim.demo;
 
 import com.nazhim.demo.model.Alien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AlienRepo extends JpaRepository<Alien, Integer> {
 
     List<Alien> findByAname(String aname);   // this method acts like a Query which is handled by the JPA
+
+    // we also can make our own queries than using the JPA method way.
+    // @Query("from Alien where aname= :name")
+    // List<Alien> find(@Param("name") String aname);
 }
