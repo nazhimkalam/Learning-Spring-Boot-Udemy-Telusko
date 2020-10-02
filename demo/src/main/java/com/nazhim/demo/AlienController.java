b98ef65215d54dedee334445a7befd206bb23953
Nazhim <nazhimkalam@gmail.com>
2020-10-03 00:45:13 +0530

    PathVariable

--- demo/src/main/java/com/nazhim/demo/AlienController.java
+++ demo/src/main/java/com/nazhim/demo/AlienController.java
@@ -4,6 +4,7 @@ import com.nazhim.demo.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -23,4 +24,13 @@ public class AlienController {
         return aliens;
     }
 
+    @GetMapping("alien/{id}")
+    @ResponseBody
+    public Alien getAlien(@PathVariable("id") int aid)
+    {
+        // if alien not present then we send a new alien with values 0 and empty string.
+        Alien alien = repo.findById(aid).orElse(new Alien(0, ""));
+
+        return alien;
+    }
 }
