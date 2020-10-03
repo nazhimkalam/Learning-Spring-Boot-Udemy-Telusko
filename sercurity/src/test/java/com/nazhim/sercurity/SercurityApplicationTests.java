e256566e7f15e62d68eac15ec200f64c7660c549
Nazhim <nazhimkalam@gmail.com>
2020-10-03 12:06:45 +0530

    Created a new application to work with spring security

---
+++ sercurity/src/test/java/com/nazhim/sercurity/SercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazhim.sercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
